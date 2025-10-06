fdc8fc6e750cb7ea6bc0fdbd648a54ce320fecb0
AtsukiProgram <atsuki.game.y@gmail.com>
2025-10-07 08:05:40 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -3,17 +3,17 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// あなたのFirebaseプロジェクト設定
 const firebaseConfig = {
-  apiKey: "あなたのAPIキー",
-  authDomain: "xxxxx.firebaseapp.com",
-  projectId: "xxxxx",
-  storageBucket: "xxxxx.appspot.com",
-  messagingSenderId: "xxxxxx",
-  appId: "1:xxxxxxx:web:xxxxxxxxxx",
-  measurementId: "G-XXXXXXXX"
+  apiKey: "AIzaSyCqBIJy19f5nuH0rnws_tU-cFbw-lUx_ko",
+  authDomain: "forums-app-6891f.firebaseapp.com",
+  projectId: "forums-app-6891f",
+  storageBucket: "forums-app-6891f.firebasestorage.app",
+  messagingSenderId: "840866364881",
+  appId: "1:840866364881:web:4ed3f6e03dea6285edf4f0"
 };
 
+// Firebase初期化・サービスエクスポート
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
