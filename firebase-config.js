9222af98451548eaac2ee9651218d7cbcd96eaf4
AtsukiProgram <atsuki.game.y@gmail.com>
2025-10-07 16:52:12 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,7 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// あなたのFirebaseプロジェクト設定
 const firebaseConfig = {
   apiKey: "AIzaSyCqBIJy19f5nuH0rnws_tU-cFbw-lUx_ko",
   authDomain: "forums-app-6891f.firebaseapp.com",
@@ -12,8 +10,6 @@ const firebaseConfig = {
   messagingSenderId: "840866364881",
   appId: "1:840866364881:web:4ed3f6e03dea6285edf4f0"
 };
-
-// Firebase初期化・サービスエクスポート
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
